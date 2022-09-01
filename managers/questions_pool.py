e9a1efc876d977ac5e10f06ceadbb566bc7bb0f7
Eythan01ch <93129649+Eythan01ch@users.noreply.github.com>
2022-09-01 14:08:46 +0300

    more questions

--- managers/questions_pool.py
+++ managers/questions_pool.py
@@ -2,6 +2,11 @@ from models.question import Question
 
 
 class QuestionsPool:
+	"""
+	The QuestionsPool class is a class that contains
+	a list of questions that are related to a subject
+	"""
+
 	def __init__(self, subject):
 		self.subject = subject
 		self.description_question = Question(
@@ -14,8 +19,23 @@ class QuestionsPool:
 			name='ORIGIN_OF_TERM',
 			max_tokens=500,
 		)
+		self.list_of_topics_related_question = Question(
+			prompt=f'give me a list of related topics about {self.subject}',
+			name='TOPICS_RELATED',
+			max_tokens=500,
+		)
+		self.simple_explanation = Question(
+			prompt=f'explain {self.subject} in the most simple way',
+			name='SIMPLE_EXPLANATION',
+			max_tokens=500,
+		)
 
-		self.questions_pool = [self.description_question, self.term_question]
+		self.questions_pool = [
+			self.description_question,
+			self.term_question,
+			self.list_of_topics_related_question,
+			self.simple_explanation,
+		]
 
 	def __str__(self):
 		return str(self.questions_pool)
